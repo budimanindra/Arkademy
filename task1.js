937929367719eb66b6cc7cbf2822e5b069f16278
Jowsevano <57140114+Jowsevano@users.noreply.github.com>
2020-12-22 12:44:58 +0700

    Tugas pertemuan ketiga arkademy

---
+++ task1.js
@@ -0,0 +1,18 @@
+const sebuahObjek = {
+    name : 'Indra Budiman',
+    age : 22,
+    hobbies : ['playing', 'reading', 'sleeping'],
+    isMarried : false,
+    schoolList : {
+        'name' : 'Mataram University',
+        'yearIn' : '2016',
+        'yearOut' : '2020',
+        'major' : 'Electrical'       
+    } ,
+    skills : {
+        'beginner' : 'Design',
+        'advanced' : 'Programming',
+        'expert' : '2020',       
+    } ,
+    interestInCoding : true
+}
