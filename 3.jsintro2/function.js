6aa50d133711a4c5af9fbdfbef654de8d4623f5a
Jowsevano <budimanindra98@gmail.com>
2020-12-29 15:05:26 +0700

    menghapus seluruh file dalam repo ini

--- 3.jsintro2/function.js
+++
@@ -1,6 +0,0 @@
-function addition(number1, number2) {
-    hasil = number1 * number2
-    console.log(hasil)
-}
-
-addition(22,2)
--- 4.jsintro3/task4.js
+++
@@ -1,5 +0,0 @@
-for (let i = 0; i <= 9; i++) {
-    fetch('https://jsonplaceholder.typicode.com/users/')
-        .then(res => res.json())
-        .then(data => console.log(data[i].name))
-}
