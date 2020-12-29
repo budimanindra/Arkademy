513f79a4fff29bb0d77b26030a193439b7fa2256
Jowsevano <budimanindra98@gmail.com>
2020-12-29 15:07:33 +0700

    mengupload ulang pertemuan kedua arkademy

---
+++ 1.algoritma/tes.js
@@ -0,0 +1,6 @@
+const arrayAsal = [ [ '4', '5', '6', '7', '8', '9' ], [ '1', '2', '3', '5', '6', '7' ] ]
+console.log(arrayAsal.join())
+console.log(arrayAsal[1][0])
+
+const angka = 321013203310141013124
+console.log(angka)
